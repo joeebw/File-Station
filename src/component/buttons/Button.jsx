bec5bb0a719fb659c42ef71215fe3bbde4b4d150
joeebw <joeegaarciia@gmail.com>
2023-04-17 20:24:43 -0600

    add remove icon to files for delete files

--- src/component/buttons/Button.jsx
+++ src/component/buttons/Button.jsx
@@ -15,10 +15,11 @@ function ButtonDashBoard(props) {
         </button>
         :
           
-        <a href={`${url}`} target='_blank' className= 'min-w-[110px] max-w-xs bg-white text-green-500 border-2 border-green-500 hover:bg-green-500 hover:text-white active:bg-green-700 active:border-green-700 p-3 rounded'
+        <a href={`${url}`} target='_blank' className= 'relative min-w-[110px] max-w-xs bg-white text-green-500 border-2 border-green-500 hover:bg-green-500 hover:text-white active:bg-green-700 active:border-green-700 p-3 rounded'
           onClick={onClick}
           title={children}
         >
+          <i class="absolute top-0 right-1 fa-solid fa-xmark"></i>
           <i className={`fa-regular fa-file mr-2`}></i>
           {children.length <= 27 ? children : `${truncatedText}...` }
         </a>
